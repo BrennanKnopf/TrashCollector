c8db1791537a2903f9cf36cf8ad0ae6694b4a154
andyrod <andyrod209@gmail.com>
2022-01-25 10:05:02 -0800

    Added employee class and on views a function to add new employee

--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Empolyee(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
+    address = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=5)
 
 # TODO: Create an Employee model with properties required by the user stories
--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from django.apps import apps
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from trash_collector import employees
 
 # Create your views here.
 
@@ -11,3 +15,16 @@ def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
     Customer = apps.get_model('customers.Customer')
     return render(request, 'employees/index.html')
+
+@login_required
+def create(request):
+    logged_in_user = request.user
+    if request.method == "POST":
+        name_from_form = request.POST.get('name')
+        address_from_form = request.POST.get('address')
+        zip_from_form = request.POST.get('zip_code')
+        new_employee = employees(name=name_from_form, user=logged_in_user, address=address_from_form, zip_code=zip_from_form, weekly_pickup=weekly_from_form)
+        new_employee.save()
+        return HttpResponseRedirect(reverse('customers:index'))
+    else:
+        return render(request, 'customers/create.html')
