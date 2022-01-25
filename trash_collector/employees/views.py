13910f3586f458f88d00694369ab861c604399ad
andyrod <andyrod209@gmail.com>
2022-01-25 11:44:08 -0800

    Created employee create page. We've migrated employee to our database.

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -5,16 +5,31 @@ from django.apps import apps
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
 from .models import Employee
+from datetime import date
 
 # Create your views here.
 
 # TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
 
 
+
+@login_required
 def index(request):
-    # This line will get the Customer model from the other app, it can now be used to query the db for Customers
-    Customer = apps.get_model('customers.Customer')
-    return render(request, 'employees/index.html')
+    # The following line will get the logged-in user (if there is one) within any view function
+    logged_in_user = request.user
+    try:
+        # This line will return the employee record of the logged-in user if one exists
+        logged_in_employee = Employee.objects.get(user=logged_in_user)
+
+        today = date.today()
+        
+        context = {
+            'logged_in_employee': logged_in_employee,
+            'today': today
+        }
+        return render(request, 'employees/index.html', context)
+    except ObjectDoesNotExist:
+        return HttpResponseRedirect(reverse('employees:create'))
 
 @login_required
 def create(request):
@@ -25,6 +40,6 @@ def create(request):
         zip_from_form = request.POST.get('zip_code')
         new_employee = Employee(name=name_from_form, user=logged_in_user, address=address_from_form, zip_code=zip_from_form)
         new_employee.save()
-        return HttpResponseRedirect(reverse('customers:index'))
+        return HttpResponseRedirect(reverse('employees:index'))
     else:
-        return render(request, 'customers/create.html')
+        return render(request, 'employees/create.html')
