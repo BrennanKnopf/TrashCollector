c6c2aaa61a2d545253a7680aa2a1f0eb02fda9f2
BrennanKnopf <brennanknopf@gmail.com>
2022-01-26 09:56:49 -0600

    Created a list for the employee to see a list of people who's trash still needs to be picked up.

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,3 +1,5 @@
+from re import A
+from time import strftime
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -7,6 +9,7 @@ from django.contrib.auth.decorators import login_required
 
 from .models import Employee
 from datetime import date
+import calendar
 from customers.models import Customer
 # Create your views here.
 
@@ -22,17 +25,17 @@ def index(request):
         # This line will return the employee record of the logged-in user if one exists
         logged_in_employee = Employee.objects.get(user=logged_in_user)
 
-        today = date.today()
-        
+        calendar_date = date.today()
+        day_of_week = date.today().strftime("%A")
         local_customers = Customer.objects.filter(zip_code = logged_in_employee.zip_code)
             #figure out how to step into next step 
             # customer name is not being added to today's trash variable 
-        todays_trash = local_customers.filter(weekly_pickup = today)
-        non_suspended_customers = todays_trash.exclude(suspend_start__gt=today, suspend_end__lt=today)
-        trash_unpicked = non_suspended_customers.filter(date_of_last_pickup__lt=today)
+        todays_trash = local_customers.filter(weekly_pickup = day_of_week)
+        non_suspended_customers = todays_trash.exclude(suspend_start__gt= calendar_date, suspend_end__lt= calendar_date)
+        trash_unpicked = non_suspended_customers.exclude(date_of_last_pickup= calendar_date)
         context = {
             'logged_in_employee': logged_in_employee,
-            'today': today,
+            'calendar_date': calendar_date,
             'trash_unpicked': trash_unpicked,
         }
         
