05aa543e1a278696ca2c22cdd1fb315a09a55331
BrennanKnopf <brennanknopf@gmail.com>
2022-01-27 17:00:50 -0600

    Changed a button on the edit profile screen.

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -14,10 +14,6 @@ from customers.models import Customer
 from django.db.models import Q
 # Create your views here.
 
-# TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
-
-
-
 @login_required
 def index(request):
     # The following line will get the logged-in user (if there is one) within any view function
@@ -27,8 +23,8 @@ def index(request):
         # This line will return the employee record of the logged-in user if one exists
         logged_in_employee = Employee.objects.get(user=logged_in_user)
         # Customer = apps.get_model('customers.Customer')
-        calendar_date = date.today()
-        day_of_week = date.today().strftime("%A")
+        calendar_date = date.today() #creates a numeric date to deal with filtering one day pickup and service suspension
+        day_of_week = date.today().strftime("%A") #creates a string for the day of the week associated with todays date.
         local_customers = Customer.objects.filter(zip_code = logged_in_employee.zip_code) 
         todays_trash = local_customers.filter(Q(weekly_pickup = day_of_week) | Q(one_time_pickup=calendar_date))
         non_suspended_customers = todays_trash.exclude(suspend_start__gt= calendar_date, suspend_end__lt= calendar_date)
@@ -36,7 +32,7 @@ def index(request):
         context = {
             'logged_in_employee': logged_in_employee,
             'calendar_date': calendar_date,
-            'trash_unpicked': trash_unpicked,
+            'trash_unpicked': trash_unpicked, #final list used in html to print names scheduled for pick up that day
         }
 
         return render(request, 'employees/index.html', context)
